8ec3c67e7879be21aae50627887a7dc269e3cb08
Harisankar Sadasivan <hsadasivan@nvidia.com>
2022-01-20 14:40:47 -0800

    partial commit

--- src/include/common.hpp
+++ src/include/common.hpp
@@ -7,3 +7,7 @@ typedef uint64_t index_t; // data type for indices
 typedef uint8_t label_t;  // data type for label
 #define GROUP_SIZE 32
 //...............global variables..........................//
+
+#define QUERY_LEN 1024
+#define BLOCK_NUM 1344
+#define REF_LEN QUERY_LEN *BLOCK_NUM
--- src/main.cu
+++ src/main.cu
@@ -26,7 +26,7 @@ using namespace FullDTW;
   cudaEventSynchronize(stop##label);                                           \
   cudaEventElapsedTime(&time##label, start##label, stop##label);               \
   std::cout << "TIMING: " << time##label << " ms "                             \
-            << ((num_features + 1) * (num_features + 1) * num_entries *        \
+            << ((query_len + 1) * (query_len + 1) * num_entries *              \
                 num_entries) /                                                 \
                    (time##label * 1e6)                                         \
             << " GCUPS (" << #label << ")" << std::endl;
@@ -35,50 +35,62 @@ using namespace FullDTW;
 int main(int argc, char *argv[]) {
 
   TIMERSTART(malloc)
-  index_t num_entries = 1344;  // number of sequences
-  index_t num_features = 1024; // length of all sequences
+  index_t num_entries = BLOCK_NUM; // number of sequences
+  index_t query_len = QUERY_LEN;   // length of all sequences
 
   /* count total cell updates */
-  const value_t CU = num_features * num_features * num_entries * num_entries;
+  const value_t CU = query_len * query_len * num_entries * num_entries;
   std::cout << "We are going to process " << CU / 1000000000.0
             << " Giga Cell Updates (GCU)" << std::endl;
 
   // create host storage and buffers on devices
-  value_t *data_cpu = nullptr, // time series on CPU
-      *dist_cpu = nullptr,     // distance results on CPU
-      *data_gpu,               // time series on GPU
-      *dist_gpu;               // distance results on GPU
-  cudaMallocHost(&data_cpu,
-                 sizeof(value_t) * num_entries * num_features); /* input */
-  cudaMalloc(&data_gpu, sizeof(value_t) * num_entries * num_features);
-  cudaMallocHost(&dist_cpu,
+  value_t *host_query = nullptr, // time series on CPU
+      *host_dist = nullptr,      // distance results on CPU
+      *device_query,             // time series on GPU
+      *device_dist,              // distance results on GPU
+          *host_ref = nullptr, *device_ref = nullptr;
+  //----host mem allocation----------------//
+  cudaMallocHost(&host_query,
+                 sizeof(value_t) * num_entries * QUERY_LEN); /* input */
+  cudaMallocHost(&host_ref, sizeof(value_t) * REF_LEN);      /* input */
+  cudaMallocHost(&host_dist,
                  sizeof(value_t) * num_entries * num_entries); /* results */
-  cudaMalloc(&dist_gpu, sizeof(value_t) * num_entries * num_entries);
+
+  //-------dev mem allocation----------//
+  cudaMalloc(&device_query, sizeof(value_t) * num_entries * query_len);
+
+  cudaMalloc(&device_ref, sizeof(value_t) * num_entries * num_entries);
+  cudaMallocHost(&host_dist, sizeof(value_t) * num_entries * num_entries);
+  cudaMalloc(&device_dist, sizeof(value_t) * num_entries * num_entries);
   CUERR
   TIMERSTOP(malloc)
 
   /* load data from memory into CPU array, initialize GPU results */
   TIMERSTART(load_data)
-  generate_cbf(data_cpu, num_features, num_entries);
-  // load_binary(data_cpu, num_features * num_entries,
+  generate_cbf(host_query, query_len, num_entries);
+  // load_binary(host_dist, query_len * num_entries,
   //             "../../../data/kernel/dtw_car.bin");
-  cudaMemcpyAsync(data_gpu, data_cpu,
-                  sizeof(value_t) * num_features * num_entries,
+  cudaMemcpyAsync(device_query, host_query,
+                  sizeof(value_t) * query_len * num_entries,
                   cudaMemcpyHostToDevice);
+  cudaMemcpyAsync(device_ref,
+                  host_query, //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
+                  sizeof(value_t) * REF_LEN, cudaMemcpyHostToDevice);
   CUERR
-  cudaMemsetAsync(dist_gpu, 0, sizeof(value_t) * num_entries * num_entries);
+  cudaMemsetAsync(device_dist, 0, sizeof(value_t) * num_entries * num_entries);
   CUERR
   TIMERSTOP(load_data)
 
   /* perform pairwise DTW computation */
   TIMERSTART_CUDA(computation)
-  distances(data_gpu, dist_gpu, num_features, num_entries, (float)0.0);
+  distances(device_ref, device_query, device_dist, query_len, num_entries,
+            (float)0.0);
   CUERR
   TIMERSTOP_CUDA(computation)
 
   /* copy results to cpu */
   TIMERSTART(save_data)
-  cudaMemcpyAsync(dist_cpu, dist_gpu,
+  cudaMemcpyAsync(host_dist, device_dist,
                   sizeof(value_t) * num_entries * num_entries,
                   cudaMemcpyDeviceToHost);
   CUERR
@@ -89,7 +101,7 @@ int main(int argc, char *argv[]) {
   // std::cout << "RESULTS:" << std::endl;
   for (int i = 0; i < num_entries; i++) {
     for (int j = 0; j < num_entries; j++) {
-      std::cout << dist_cpu[i * num_entries + j] << " ";
+      std::cout << host_dist[i * num_entries + j] << " ";
     }
     std::cout << std::endl;
   }
@@ -97,13 +109,13 @@ int main(int argc, char *argv[]) {
 #endif
 
   TIMERSTART(free)
-  cudaFree(data_gpu);
+  cudaFree(device_dist);
   CUERR
-  cudaFree(dist_gpu);
+  cudaFree(device_dist);
   CUERR
-  cudaFreeHost(data_cpu);
+  cudaFreeHost(host_query);
   CUERR
-  cudaFreeHost(dist_cpu);
+  cudaFreeHost(host_dist);
   CUERR
   TIMERSTOP(free)
 
